a900898000eb9be26f96633e7635e42b66ca59f8
Srikanth K S <sri.teach@gmail.com>
2020-05-02 20:30:43 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,56 +1,67 @@
-# dplyr count like utility: returns an ungrouped dataframe
-def tidy_count(df, count_column_name = "n"):
-  
-  assert (df.__class__.__name__ == "DataFrameGroupBy"), "input should be a grouped dataframe"
-  
-  res = df.size().reset_index()
-  groupby_vars = df.grouper.names
-  if count_column_name in groupby_vars:
-    raise Exception("count column name should be different from groupby variable names")
-  res.rename(columns = {0: count_column_name}, inplace = True)
+#---------------------------------------------
+def is_pandas_frame(df):
     
-  return res
-
-# dplyr add_count like utility: returns an ungrouped dataframe   
-def tidy_add_count(df, count_column_name = "n"):
-  
-  assert (df.__class__.__name__ == "DataFrameGroupBy"), "input should be a grouped dataframe"
-  
-  count_frame  = dplyr_count(df)
-  groupby_vars = df.grouper.names
-  column_vars  = df.obj.columns.to_list()
-  if count_column_name in column_vars:
-    raise Exception("count column name should be different from column names of grouped input dataframe")
-  
-  res = pd.merge(df.obj, count_frame, on = df.grouper.names)
-  return res
-  
-# dplyr ungroup like utility: returns an ungrouped dataframe
-def tidy_ungroup(df):
-  
-  df_class = df.__class__.__name__
-  if df_class == 'DataFrame':
-    res = df
-  else:
-    assert (df.__class__.__name__ == "DataFrameGroupBy"), "input should be a grouped dataframe"
-    res = df.obj
-  
-  return res
-
-# sanitize_index
-# 1. Converts row indexes into columns (if any)
-# 2. Flattens multilevel column indexes into single index with '__' used for concatenation
-def sanitize_index(df):
-  df = df.reset_index()
-  df.columns = ['__'.join(col).rstrip("__") for col in df.columns.values]
-  
-  return df
-
-# tidyr::complete
-def tidy_complete(df, nest_by):
-    df  = df.set_index(nest_by)
-    mux = pd.MultiIndex.from_product(df.index.levels, names = nest_by)
-    df  = df.reindex(mux, fill_value = 0).reset_index()
+    res = df.__class__.__name__ in ["DataFrame", "DataFrameGroupBy"]
     
-    return df
- 
+    return(res)
+
+#---------------------------------------------
+def is_tidy_frame(df):
+    
+    row_flag = not isinstance(training.index, pd.MultiIndex)
+    col_flag = not isinstance(training.columns, pd.MultiIndex)
+  
+    res = row_flag and col_flag
+    
+    return(res)
+
+#---------------------------------------------  
+def is_grouped_frame(df):
+  res = df.__class__.__name__ == "DataFrameGroupBy"
+  return(res)
+
+#---------------------------------------------
+def ungroup(df):
+    assert is_pandas_frame(df)
+    assert is_tidy_frame(df)
+    if is_grouped_frame(df):
+        res = df.obj
+    else:
+        res = df
+    return res
+
+#---------------------------------------------
+def count(df, column_names_to_group_by = [], count_column_name = "n"):
+  
+    assert is_pandas_frame(df)
+    assert is_tidy_frame(df)
+      
+    if isinstance(column_names_to_group_by, str):
+        temp_list = []
+        temp_list.append(column_names_to_group_by)
+        column_names_to_group_by = temp_list
+        
+    grouped_flag = is_grouped_frame(df)
+  
+    if grouped_flag:
+        group_vars   = df.grouper.names
+        df_ungrouped = df.obj
+    else:
+        group_vars   = []
+        df_ungrouped = df
+         
+    new_group_vars = list(set(group_vars + column_names_to_group_by))
+  
+    if count_column_name in new_group_vars:
+        raise Exception("count column name should be different from groupby variable names")
+  
+    # count by looking at grouped sizes  
+    res = (df_ungrouped.groupby(new_group_vars)
+                       .size()
+                       .reset_index()
+                       .rename(columns = {0: count_column_name}))
+  
+    if grouped_flag:
+        res = res.groupby(group_vars)
+  
+    return res
