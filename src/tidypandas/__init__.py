ca152c1c4b85f61dfed76a1459c346ab31562364
talegari <sri.teach@gmail.com>
2022-04-07 22:37:54 +0530

    rename of main py file + build

--- src/tidypandas/__init__.py
+++ src/tidypandas/__init__.py
@@ -1,4 +1,4 @@
-from tidypandas.tidypandas import tidyframe
+from tidypandas.tidyframe_class import tidyframe
 from tidypandas import series_utils
 from tidypandas import tidy_utils
 from tidypandas import tidy_accessor
